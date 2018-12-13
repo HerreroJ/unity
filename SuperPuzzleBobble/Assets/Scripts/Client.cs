7747fe68aa74b338b73e4fdb35758913663e5381
Jon Herrero <45487254+HerreroJ@users.noreply.github.com>
2018-12-13 13:40:51 +0100

    asd

---
+++ SuperPuzzleBobble/Assets/Scripts/Client.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player {
+    public string playerName;
+    public int posJugador;
+    public GameObject avatar;
+    public int connectId;
+}
+
+public class Client : MonoBehaviour {
+
+    private const int MAX_CONNECTION = 100;
+    private int port = 5701;
+
+    private int hostId;
+    private int webHostId;
+
+    private int reliableChannel;
+    private int unReliableChannel;
+
+    private float connectionTime;
+    private int connectionId;
+    private bool isConnected;
+    private bool isStarted = false;
+
+    private byte error;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
